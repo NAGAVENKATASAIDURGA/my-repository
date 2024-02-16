30ab9ab671e616363e2c359b2d4d72a8401e23ef
nagavenkatasaidurgaposimsetti <saidurga.posimsetty@gmail.com>
2024-02-15 23:41:02 -0800

    updated sample.java file here

--- Sample.java
+++ Sample.java
@@ -1,6 +1,5 @@
 class Sample{
-public static void main(String args[])
-{
-System.out.pritnln("Gitlab Testing");
+public static void main(String args[]){
+System.out.println("Hello this is my first program in Java");
 }
 }
